08f270edd54890a68ed047639e0e4291aa0c2585
xuan <2274409207@qq.com>
2024-11-06 21:51:10 +0800

    Initial Commit

---
+++ src/main/java/com/example/learninggitidea/DevClass.java
@@ -0,0 +1,15 @@
+package com.example.learninggitidea;
+
+/**
+ * @BelongsProject: learninggitidea
+ * @BelongsPackage: com.example.learninggitidea
+ * @Author: xuan
+ * @CreateTime: 2024-11-06  19:42
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class DevClass {
+    public static void main(String[] args) {
+        System.out.println("DevClass");
+    }
+}
