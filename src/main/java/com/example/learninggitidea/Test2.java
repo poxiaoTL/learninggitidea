68aea736a85acc61f435f1202fa1fbbef74f44bf
xuan <xi2274409207@gmail.com>
2024-12-01 20:25:04 +0800

    add test2

--- src/main/java/com/example/learninggitidea/Test2.java
+++ src/main/java/com/example/learninggitidea/Test2.java
@@ -1,8 +1,13 @@
-package com.example.learninggitidea;/**
-*@Author: xuan
-*@CreateTime: 2024-12-01  20:17
-*@Description: TODO
-*/
+package com.example.learninggitidea;
+
+/**
+ * @Author: xuan
+ * @CreateTime: 2024-12-01  20:17
+ * @Description: TODO
+ */
 
 public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("这是测试2");
+    }
 }
